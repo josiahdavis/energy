a17383cd64fb30183c9b8ad3f779fd5bfae2a5e4
Aaron Hardisty <aaron.hardisty@gmail.com>
2015-10-09 16:17:49 -0700

    updated data with CDD, HDD and median temp

--- UtilConsumption.R
+++ UtilConsumption.R
@@ -11,38 +11,25 @@ lapply(loadP, library, character.only = TRUE)
 # meter sample data
 meterSample <- read.csv("smallelectricitytimeofusedataset.csv", header = TRUE, check.names = TRUE, 
                         strip.white = TRUE,stringsAsFactors = FALSE)
-# head(meterSample)
-# sapply(meterSample, class)
-# head(meterSample)
-# meterTest <- meterSample[1:20,]
 
 # customer infomation sample data
-customerData <- read.csv("../../Dropbox/DropData/PowerData/SGSC-CTCustomer-Household-Data.csv", header = TRUE)
-# str(customerData)
-# head(customerData)
-# summary(customerData$POSTCODE)
-# length(table(as.factor(customerData$POSTCODE)))
-# length(table(customerData$SUBURB_NAME))
+customerData <- read.csv("SGSC-CTCustomer-Household-Data.csv", header = TRUE)
 
 # offer response data
-customerOffers <- read.csv("../../Dropbox/DropData/PowerData/SGSC-CTOffers-and-Acceptances.csv", header = TRUE)
+customerOffers <- read.csv("SGSC-CTOffers-and-Acceptances.csv", header = TRUE)
 
 # daily meter reading
-dailyReads <- read.csv("../../Dropbox/DropData/PowerData/energyWide.csv", header = TRUE)
-summary(dailyReads)
+dailyReads <- read.csv("energyWide.csv", header = TRUE)
 
-# load historic weather data Sydney (Observatory Hill)
+## load historic weather data Sydney (Observatory Hill)
 # 66062 Lat: 33.86° SLon: 151.21° EElevation: 39 m
 
-tempHistoricMax <- read.csv("../../Dropbox/DropData/PowerData/IDCJAC0010_066062_1800/IDCJAC0010_066062_1800_Data.csv")
-tempHistoricMin <- read.csv("../../Dropbox/DropData/PowerData/IDCJAC0011_066062_1800/IDCJAC0011_066062_1800_Data.csv")
-
+tempHistoricMax <- read.csv("IDCJAC0010_066062_1800_Data.csv")
+tempHistoricMin <- read.csv("IDCJAC0011_066062_1800_Data.csv")
 
 # Clean Sample Data -------------------------------------------------------
 
 # meter sample data
-str(meterSample)
-summary(meterSample)
 meterSample$FullTime <- strptime(meterSample$End.Datetime, format = "%d/%m/%Y %H:%M")
 as.Date(meterSample$End.Datetime, format = "%d/%m/%Y")
 meterSample$Year <- year(meterSample$FullTime)
@@ -50,76 +37,66 @@ meterSample$Year <- ifelse(nchar(meterSample$Year) == 2, paste("20",year(meterSa
 meterSample$Month <- month(meterSample$FullTime)
 meterSample$Day <- day(meterSample$FullTime)
 
-# change CUSTOMER_ID to factor
-meterSample$CUSTOMER_KEY <- factor(meterSample$CUSTOMER_KEY)
+meterSample$CUSTOMER_KEY <- factor(meterSample$CUSTOMER_KEY) # change customer key to factor
 
 meterGroup <- meterSample %>% 
-  select(-FullTime) %>% 
+  select(-FullTime) %>% # drop time data
   group_by(CUSTOMER_KEY, Year, Month) %>% 
   select(CUSTOMER_KEY, Month, General.Supply.KWH:Net.Generation.KWH) %>% 
   summarise(n = n())
 
 list(unique(meterSample$CUSTOMER_KEY)) # find unique customer keys
-summary(as.factor(meterSample$Year))
 
-# customer infomation sample data
-head(customerData)
-# list(unique(customerData$CUSTOMER_KEY))
 customerDataSub <- customerData[customerData$CUSTOMER_KEY %in% meterSample$CUSTOMER_KEY,] #subset on customers in meter samples
 
-customerDataSubRank  <-  customerDataSub %>% 
-  group_by(TRIAL_REGION_NAME) %>% 
-  summarize(count = n()) %>% 
+customerDataSubRank  <-  customerDataSub %>% # create groupwise summary of customer data
+  group_by(TRIAL_REGION_NAME) %>% # find most common 
+  summarize(count = n()) %>% # summarize
   arrange(desc(count))
 
-# list(customerDataSubRank$TRIAL_REGION_NAME)
 
-# customerDataSubRank  <-  customerDataSub %>% 
-#   group_by(POSTCODE) %>% 
-#   summarize(postCount = n()) %>% 
-#   arrange(desc(postCount))
-# head(customerDataSub)
+## clean historic weather data Sydney (Observatory Hill)
 
-# summary(customerDataSub)
+tempHistoricMax <- read.csv("IDCJAC0010_066062_1800_Data.csv")
+tempHistoricMin <- read.csv("IDCJAC0011_066062_1800_Data.csv")
 
-# formula that should be an anonymous function
-tempConvert <- function(x){
-  x <- round(x*(9/5) + 32,digits = 0)
-  x
-}
+tempHistoricCombine <- tempHistoricMax %>% 
+  left_join(tempHistoricMin,by = c("Year","Month","Day")) %>% # join on YMD
+  filter(Year >= 2012) %>% # filter greater than 2012
+  select(stationNumber = Bureau.of.Meteorology.station.number.x,Year, Month, Day, Maximum.temperature..Degree.C., Minimum.temperature..Degree.C.) %>%
+  mutate(MaxTempF = round(Maximum.temperature..Degree.C.*(9/5) + 32,digits = 0), # convert to F
+         MinTempF = round(Minimum.temperature..Degree.C.*(9/5) + 32,digits = 0)) %>% # convert to F
+  mutate(baseCDD = 80, # constant for CDD
+         baseHDD = 60) %>% # constant for HDD
+  mutate(CDD = ifelse(MaxTempF <= baseCDD, 0, # calculate CDD
+                       ifelse(MaxTempF > baseCDD & MinTempF <= baseCDD,
+                              (MaxTempF - baseCDD) / (2*(MaxTempF - MinTempF)),
+                              (MaxTempF + MinTempF) / (2)-baseCDD))) %>%
+  mutate(HDD = ifelse(MinTempF >= baseHDD, 0, # calculate HDD
+                      ifelse(MinTempF < baseHDD & MaxTempF >= baseHDD,
+                             (baseHDD - MinTempF) / (2*(MaxTempF - MinTempF)),
+                             (baseHDD - (MaxTempF + MinTempF)/2)))) %>% 
+  select(stationNumber, Year, Month, Day, MaxTempF, MinTempF, CDD, HDD) # select columns of interest
 
-
-
-
-# Combine Data Sets -------------------------------------------------------
-
+# Mutate -------------------------------------------------------
 
 # sydney observatory data (min and max temperature)
-tempHistoricSub <- tempHistoricMax %>% 
-  left_join(tempHistoricMin,by = c("Year","Month","Day")) %>% # join max and min on ymd
-  filter(Year >= 2012) %>% # years greater than 2012
-  rename(MaxTemp_C = Maximum.temperature..Degree.C.,MinTemp_C = Minimum.temperature..Degree.C.) %>% # shorten name
-  select(stationNumber = Bureau.of.Meteorology.station.number.x, # select grouping columns
-         Month, Year,MaxTemp_C, MinTemp_C) %>% 
-  mutate(MaxTempF = tempConvert(MaxTemp_C),
-         MinTempF = tempConvert(MinTemp_C)) %>% 
+tempHistoricSub <- tempHistoricCombine %>% 
   mutate(month_Year = paste(Month, Year, sep = "-")) %>% # create new variables
   group_by(month_Year) %>% # group by year and month year dummy
-  summarise(max_Monthly = max(MaxTempF),
-            min_Monthly = min(MinTempF),
-            median_High = median(MaxTempF),
-            median_Low = round(median(MinTempF),0),
-            measured_Days = n(),
+  summarise(max_Monthly = max(MaxTempF), # find highest temp per month
+            min_Monthly = min(MinTempF), # find lowest temp per month
+            hdd = sum(HDD), # sum hdd
+            cdd = sum(CDD), # sum cdd
+            median_temp = median(c(MaxTempF, MinTempF)), #median daily temp
+            median_High = median(MaxTempF), # median high
+            median_Low = round(median(MinTempF),0), # median low
+            measured_Days = n(), # measured days per month
             days_80 = sum(MaxTempF > 80), # days max temp was above 80
             days_65 = sum(MinTempF < 65)) %>% # days max temp was below 65
   arrange(month_Year)
+head(tempHistoricSub)
 
-
-  # cooling degree days
-  # if max temp for month is less than base then cooling degrees == 0
-  # if max temp > base and minimum temp <= base
-  
-tempHistoricSub # view data frame
 
 # list(unique(meterSample$CUSTOMER_KEY)) # 30 unique households
 # dim(meterSample[meterSample$CUSTOMER_KEY == "8170837",] ) # 15977 rows
@@ -137,10 +114,6 @@ meterSampleSub <- meterSample %>%
             total_KWH = general_KWH+offPeak_KWH+grossGen_KWH+netGen_KWH) %>% 
   left_join(tempHistoricSub, by = "month_Year")# join with temperature data
 
-
-
 # Export to CSV -----------------------------------------------------------
 
-write.csv(x = meterSampleSub, file = "../../Dropbox/Utility/meterSample_temp.csv",row.names = FALSE)
-list.files()
-getwd()
+write.csv(x = meterSampleSub, file = "data.csv",row.names = FALSE)
